748200d160dffc2d1348bd7da1f0805174e57b2e
yssmmr <noppie429@gmail.com>
2023-11-18 15:32:20 +0900

    ADD 修了確認　スコープ

--- javascript-essence-tutorial/030_スコープ/910_修了確認_スコープ/start/main.js
+++ javascript-essence-tutorial/030_スコープ/910_修了確認_スコープ/start/main.js
@@ -7,8 +7,10 @@
  * ※if文は削除してはいけません。
  */
 function fn() {
+    let a;
+
     if(true) {
-        let a = 'fn called';
+         a = 'fn called';
     }
     return a; // ReferenceError: a is not defined
 }
@@ -26,7 +28,7 @@ function fn2() {
     console.log(val); // 期待値->'val1'
 
     if(true) {
-        var val = 'val2';
+        let val = 'val2';
         console.log(val); // 期待値->'val2'
     }
 
@@ -46,3 +48,16 @@ fn2();
  * increment(); // 期待値->4
  */
 
+{   
+    let num = 0;
+    //increment()はブロックスコープの外からでも呼び出しできる
+    function increment() {
+        num += 1;
+        console.log(num);
+    }
+}
+
+increment();
+increment();
+increment();
+increment();
