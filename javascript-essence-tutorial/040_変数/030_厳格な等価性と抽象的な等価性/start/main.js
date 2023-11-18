d41e820ed596a5d51abc8ef02d66cbced7be8009
yssmmr <noppie429@gmail.com>
2023-11-18 21:24:05 +0900

    ADD 厳格な等価性

---
+++ javascript-essence-tutorial/040_変数/030_厳格な等価性と抽象的な等価性/start/main.js
@@ -0,0 +1,17 @@
+function printEquality(a, b) {
+  console.log(a === b);
+  console.log(a == b);  
+}
+
+let a = '1';
+let b = 1;
+let c = true;
+
+console.log(b === Number(c));
+// printEquality(b, c)の事。
+
+let e = "";
+let f = 0;
+let g = "0";
+
+printEquality(g, f);
