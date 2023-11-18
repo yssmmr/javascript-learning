08e9c8313b82e29f4a609f8fa706f1e24ae5f22e
yssmmr <noppie429@gmail.com>
2023-11-18 14:22:17 +0900

    ADD 即時関数

---
+++ javascript-essence-tutorial/030_スコープ/070_即時関数/start/main.js
@@ -0,0 +1,31 @@
+//即時関数、関数定義と同時に一度だけ実行される関数
+
+function a() {
+  console.log("called");
+}
+
+a();
+
+let c = (function(){
+  
+  console.log("called");
+  
+  let privateVal = 0;
+  let publicVal = 10;
+
+  function privateFn() {
+    console.log("privateFn is called");
+  }
+  function publicFn() {
+    console.log("publicFn is called");
+  }
+
+  return {
+    publicVal,
+    publicFn
+  };
+})()
+
+c.publicFn();
+
+console.log(c.publicVal);
