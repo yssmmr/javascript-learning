d0e7845c13990ca14a0725d90171896b29b0fe9c
yssmmr <noppie429@gmail.com>
2023-11-19 16:15:22 +0900

    ADD ANDとOR　応用

---
+++ javascript-essence-tutorial/040_変数/060_AND条件とOR条件（応用）/start/main.js
@@ -0,0 +1,11 @@
+function hello(name) {
+    name = name || 'Tom'; //nameがない場合Tomが表示されるように。
+    console.log('hello ' + name);
+  }
+
+hello();
+
+let name = 'Bob';
+
+name && hello(name);
+//nameがtruthyならばhello()に処理がうつる。
