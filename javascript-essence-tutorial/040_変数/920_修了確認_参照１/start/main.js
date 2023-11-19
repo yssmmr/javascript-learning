980aa4a28aa913eb4d177fdcd2efaf818ab79f52
yssmmr <noppie429@gmail.com>
2023-11-19 21:05:34 +0900

    ADD　修了確認　参照１

--- javascript-essence-tutorial/040_変数/920_修了確認_参照１/start/main.js
+++ javascript-essence-tutorial/040_変数/920_修了確認_参照１/start/main.js
@@ -7,7 +7,7 @@ let val = 0;
 let val2 = val;
 val2 = 1;
 
-// console.log(val); // 問題１
+console.log(val); // 問題１
 
 let obj = {
     prop1: 10
@@ -15,7 +15,7 @@ let obj = {
 const obj2 = obj;
 obj2.prop1 = 9;
 
-// console.log(obj); // 問題２
+console.log(obj); // 問題２
 
 function fn1(arg) {
     arg.prop2 = 8;
@@ -23,7 +23,7 @@ function fn1(arg) {
 }
 let obj3 = fn1(obj2);
 
-// console.log(obj); // 問題３
+console.log(obj); // 問題３
 
 function fn2(arg) {
     arg = {};
@@ -31,4 +31,4 @@ function fn2(arg) {
 }
 const obj4 = fn2(obj); // 問題４
 
-// console.log(obj); // 問題４
+console.log(obj); // 問題４
