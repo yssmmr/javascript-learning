872691bc0dfd177c2c74c27f508c420b5d7bb53c
yssmmr <noppie429@gmail.com>
2023-11-24 22:50:06 +0900

    ADD this

--- javascript-essence-tutorial/050_関数とオブジェクト１/040_this/start/main.js
+++ javascript-essence-tutorial/050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,11 @@
+//this→呼び出し元のオブジェクトへの参照を保持するキーワード
+//thisの参照先は実行コンテキストによって変わる
+
 const person = {
     name: 'Tom',
     hello: function() {
-
+        console.log('Hello ' + this.name) //personのオブジェクトnameを呼び出す。
     }
 }
+
+person.hello();
