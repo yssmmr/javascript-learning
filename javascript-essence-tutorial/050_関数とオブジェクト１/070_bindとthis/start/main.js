c2201dd6ec34aa44bb4d7f6e41213448bd1f39a8
yssmmr <noppie429@gmail.com>
2023-11-25 13:22:05 +0900

    bindとthis

--- javascript-essence-tutorial/050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ javascript-essence-tutorial/050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -1,3 +1,6 @@
+//bindを使用するとthisの取りうる参照を意図的に変更可能
+//引数を固定することも可能。
+
 window.name = 'John';
 
 const person = {
@@ -8,8 +11,18 @@ const person = {
 }
 person.hello();
 
+const helloTom = person.hello.bind(person); //thisの参照先をpersonに変更している
+
 function fn(ref) {
     ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+function a(name) {
+    console.log('hello ' + name);
+}
+
+const b = a.bind(null, 'Tim');
+
+b();
