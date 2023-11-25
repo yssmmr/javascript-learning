008a0392da6ddb8f58a848e4cdf9bf481069d35d
yssmmr <noppie429@gmail.com>
2023-11-25 13:48:26 +0900

    ADD call,applyとthis

--- javascript-essence-tutorial/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ javascript-essence-tutorial/050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,20 @@
-function a(name) {
-    console.log('hello ' + name);
+//call,apply→thisや引数の参照先を変更。同時に関数を実行する。
+
+function a(name, name1) {
+    console.log('hello ' + name + ' ' + name1);
 }
 
-const b = a.bind(null, 'Tim');
+const tim = {name: 'Tim'};
+
+const b = a.bind(tim);
 
 b();
+
+a.apply(tim, ['Tim', 'Bob']); //関数が実行まで行われる。第二引数が与えられると配列が与えられる。
+a.call(tim, 'Tim','Bob');
+
+
+const arry = [1, 2, 3, 4, 5];
+
+const result = Math.max(...arry); //Math.max()一番大きな数字が返ってくる
+console.log(result);
