8079f6adf84ce36d02f2fa215456a4de8839be6b
yssmmr <noppie429@gmail.com>
2023-12-01 22:19:26 +0900

    new演算子

---
+++ javascript-essence-tutorial/060_関数とオブジェクト２/030_new演算子/start/main.js
@@ -0,0 +1,19 @@
+function F(a, b) {
+  this.a = a;
+  this.b = b;
+  return {}
+}
+F.prototype.c = function() {}
+
+function newOpe(C, ...args){
+  const _this = Object.create(C.prototype);
+  const result = C.apply(_this, args);
+  console.log(typeof result);
+  if(typeof result === "object" && result !== null) {
+    return result;
+  }
+  return _this;
+}
+
+const instance = newOpe(F, 1, 2);
+//console.log(instance)
