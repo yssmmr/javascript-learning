73c097c2d9c546df338443dfdb480de9e9b14a3d
yssmmr <noppie429@gmail.com>
2023-12-03 13:34:18 +0900

    UPDATE 文章を取得＆表示

--- javascript-essence-tutorial/youtube/typegame/script.js
+++ javascript-essence-tutorial/youtube/typegame/script.js
@@ -1,4 +1,6 @@
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+const typeDisplay = document.getElementById("typeDisplay")
+//idがtypeDisplayの要素を取得する。
 
 //　非同期でランダムな文章を取得する。
 function GetRandomSentence() {
@@ -8,10 +10,15 @@ function GetRandomSentence() {
 }
 
 // ランダムな文章を取得して表示する
+//「async」は「function」の前に記述するだけで非同期処理を実行できる関数を定義
+//「await」はPromise処理の結果が返ってくるまで一時停止してくれる
+//「await」は「async」で定義された関数の中だけでしか使えない
 
 async function RenderNextSentence() {
   const sentence = await GetRandomSentence();
   console.log(sentence);
+
+  typeDisplay.innerText = sentence;
 }
 
 RenderNextSentence();
