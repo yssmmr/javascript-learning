2533ebe31e7709dc8b7fe16cab1b6bb198b1d941
yssmmr <noppie429@gmail.com>
2023-12-03 11:37:27 +0900

    ADD instanceof

--- javascript-essence-tutorial/060_関数とオブジェクト２/040_instanceof/start/main.js
+++ javascript-essence-tutorial/060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,14 @@
+//instanceof、どのコンストラクターから生成されたオブジェクトかを確認する
+//オブジェクトがプロトタイプにコンストラクタのprototypeプロパティを持っているかを確認する演算子
+
 function F(a, b) {
     this.a = a;
     this.b = b;
-    // return {a: 1};
+    return {a: 1};
 }
 
 F.prototype.c = function() {}
+
+const instance = new F(1, 2);
+console.log(instance instanceof F); //(判定したいオブジェクト instanceof オブジェクト名称)
+console.log(instance.__proto__ === F.prototype)
