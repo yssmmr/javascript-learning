fefc7de1698f0720f0cfedfe86f5e97cdd857d1e
yssmmr <noppie429@gmail.com>
2023-12-16 19:38:35 +0900

    ADD プロトタイプ継承

--- javascript-essence-tutorial/060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ javascript-essence-tutorial/060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -1,3 +1,5 @@
+//プロトタイプ継承→別のコンストラクター関数のプロトタイプを受け継いで、機能を流用できるようにすること。
+
 function Person(name, age) {
   this.name = name;
   this.age = age;
@@ -6,3 +8,17 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+function Japanese(name, age) {
+  Person.call(this, name, age);
+}
+
+Japanese.prototype = Object.create(Person.prototype);
+
+Japanese.prototype.hello = function() {
+  console.log("Konnichiwa" + this.name);
+}
+const taro = new Japanese("Taro", 23);
+console.log(taro);
+
+taro.hello();
